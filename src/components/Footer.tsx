c4f96cc390d92db4da10433f37af22dc21660198
Pillzen Pvt. Ltd <anopadraja@gmail.com>
2025-02-26 12:43:16 +0530

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,32 @@
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
     return (
       <footer className="bg-gray-900 text-white py-6 flex justify-between items-center px-10">
         {/* Left Side - Navigation */}
         <ul className="flex space-x-6 text-lg text-gray-300">
-          <li className="hover:text-orange-400 cursor-pointer">Home</li>
-          <li className="hover:text-orange-400 cursor-pointer">Process</li>
-          <li className="hover:text-orange-400 cursor-pointer">Project</li>
-          <li className="hover:text-orange-400 cursor-pointer">Contact</li>
+          <li>
+            <Link to="/" className="hover:text-orange-400">Home</Link>
+          </li>
+          <li>
+            <Link to="/process" className="hover:text-orange-400">Process</Link>
+          </li>
+          <li>
+            <Link to="/project" className="hover:text-orange-400">Project</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:text-orange-400">Contact</Link>
+          </li>
         </ul>
-  
+
         {/* Center - Logo */}
-        <div className="flex items-center justify-center">
+        <Link to="/">
           <img 
             src="/Echoes Logo.png"
             alt="Echoes Logo"
             className="w-32 h-auto"
           />
-        </div>
+        </Link>
   
         {/* Right Side - Copyright */}
         <p className="text-gray-400 text-sm">
